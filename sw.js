0140af06206a3c2d91acdd8ed3fd107caf5cf077
dcushman13 <danielrcushman@gmail.com>
2026-09-13 11:21:40 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+var CACHE = "barbell-log-v1";
+var FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES); }).catch(function(){}));
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){ return k !== CACHE; })
+      .map(function(k){ return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e){
+  if(e.request.method !== "GET") return;
+  var url = new URL(e.request.url);
+  if(url.origin !== location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then(function(hit){
+      if(hit) return hit;
+      return fetch(e.request).then(function(res){
+        var copy = res.clone();
+        caches.open(CACHE).then(function(c){ c.put(e.request, copy); }).catch(function(){});
+        return res;
+      }).catch(function(){ return caches.match("./index.html"); });
+    })
+  );
+});
