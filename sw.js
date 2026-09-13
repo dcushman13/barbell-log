9bee20e8221d5c1c677d9e4c8837132601325a6b
dcushman13 <danielrcushman@gmail.com>
2026-09-13 12:19:12 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "barbell-log-v2";
+var CACHE = "barbell-log-v3";
 var FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e){
